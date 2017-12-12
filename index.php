0a9f7a71da31265aa3fc45d44a51e003b0f07634
Anthony <anthony_bouillon@outlook.fr>
2017-12-12 16:07:13 +0100

    Les variables

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-$name = 'L\'énoncé était "créer une viariable nom" mais normalement le nom de la variable doit-etre en anglais';
-echo $name;
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <?php
+    $name = 'L\'énoncer était de  "créer une viariable nom" mais normalement le nom de la variable doit-etre en anglais';
+    echo $name;
+    ?>
+  </body>
+</html>
